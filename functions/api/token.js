5f05d3170e5bb93b0f27e45ca472e4fd5f74edf9
FranLafuente-dev <lafuentefranciscolucas@gmail.com>
2026-04-17 18:00:16 -0300

    Create token.js

---
+++ functions/api/token.js
@@ -0,0 +1,23 @@
+export async function onRequestPost(context) {
+    const body = await context.request.text();
+    const resp = await fetch('https://api.mercadolibre.com/oauth/token', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded', 'Accept': 'application/json' },
+      body
+    });
+    const data = await resp.text();
+    return new Response(data, {
+      status: resp.status,
+      headers: { 'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*' }
+    });
+  }
+
+  export async function onRequestOptions() {
+    return new Response(null, {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST',
+        'Access-Control-Allow-Headers': 'Content-Type, Accept',
+      }
+    });
+  }
